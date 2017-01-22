0b135d821052e5fcc4c4aa715067c8991b8d492b
Dominik Bielatowicz <dominikbielatowicz92@gmail.com>
2017-01-22 19:49:46 +0100

    sprawdzanie zajętości rezerwacji

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,7 +4,11 @@ class ReservationsController < ApplicationController
   # GET /reservations
   # GET /reservations.json
   def index
-    @reservations = Reservation.all
+    @reservations = Reservation.where(:room_id => Room.first.id)
+    @rooms = Room.all
+    if (params.has_key?(:room_id))
+      @reservations = Reservation.where(:room_id => room_params[:room_id])
+    end
   end
 
   # GET /reservations/1
@@ -24,16 +28,52 @@ class ReservationsController < ApplicationController
   # POST /reservations
   # POST /reservations.json
   def create
-    @reservation = Reservation.new(reservation_params)
+    @p = reservation_params
+    @rezerwacje = Reservation.where(:room_id=>reservation_params[:room_id])
+    @ds = DateTime.new(reservation_params["date_start(1i)"].to_i,
+                       reservation_params["date_start(2i)"].to_i,
+                       reservation_params["date_start(3i)"].to_i,
+                       reservation_params["date_start(4i)"].to_i,
+                       reservation_params["date_start(5i)"].to_i)
+    @dk = DateTime.new(reservation_params["date_stop(1i)"].to_i,
+                       reservation_params["date_stop(2i)"].to_i,
+                       reservation_params["date_stop(3i)"].to_i,
+                       reservation_params["date_stop(4i)"].to_i,
+                       reservation_params["date_stop(5i)"].to_i)
+    test = 0
+    if @ds < @dk
+      @rezerwacje.each do |rezerwacje|
+        @dsa = rezerwacje.date_start
+        @dka = rezerwacje.date_stop
+        if @ds > @dsa and @ds < @dka
+          test = 1
+          break
+        end
 
-    respond_to do |format|
-      if @reservation.save
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
-        format.json { render :show, status: :created, location: @reservation }
-      else
-        format.html { render :new }
-        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+        if @dk > @dsa and @dk < @dka
+          test = 1
+          break
+        end
+
+        if @ds <= @dsa and @dk >= @dka
+          test = 1
+          break
+        end
       end
+      if test == 0
+        @reservation = Reservation.new(reservation_params)
+        respond_to do |format|
+          if @reservation.save
+            format.html { redirect_to @reservation, notice: 'Rezerwacja utworzona pomyślnie.' }
+            format.json { render :show, status: :created, location: @reservation }
+          else
+            format.html { render :new }
+            format.json { render json: @reservation.errors, status: :unprocessable_entity }
+          end
+        end
+      else redirect_to new_reservation_path, alert: 'Sala zajęta.'
+      end
+    else redirect_to new_reservation_path, alert: 'Data początku wieksza lub równa niż data końca.'
     end
   end
 
